304029a27cb726f67470efd65c04cb6e12baf2be
webflyx.valuation <mid>
2026-07-04 05:43:15 -0700

    basic pygame

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
 import constants
+from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for events in pygame.event.get():
+            pass
+
+        screen.fill("black")
+        pygame.display.flip()
+        
+    # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    # print(f"Screen width: {constants.SCREEN_WIDTH}")
+    # print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
