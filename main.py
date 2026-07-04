121fe7f45563d23045072b4a036c942b77d9ef50
webflyx.valuation <mid>
2026-07-04 05:45:28 -0700

    feat: added quit event

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
     while True:
-        for events in pygame.event.get():
-            pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
         screen.fill("black")
         pygame.display.flip()
