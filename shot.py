4a485e95d02ae335de1d7830faa0286615ef1fa6
maik <michaelaykay@gmail.com>
2026-07-23 09:03:43 -0700

    added a laser shot instead of a circle shot

--- shot.py
+++ shot.py
@@ -3,11 +3,14 @@ import constants
 import circleshape
 
 class Shot(circleshape.CircleShape):
-    def __init__(self, x: float, y: float) -> None:
+    def __init__(self, x: float, y: float, rotation) -> None:
         super().__init__(x, y, constants.SHOT_RADIUS)
+        self.laser = pygame.image.load("assets/laser.png").convert_alpha()
 
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.circle(screen, "white", self.position, self.radius, constants.LINE_WIDTH)
+        # pygame.draw.circle(screen, "white", self.position, self.radius, constants.LINE_WIDTH)
+        centered_rect = self.laser.get_rect(center=self.position)
+        screen.blit(self.laser, centered_rect)
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
